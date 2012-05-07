35c933b72d37905bcf9cf2cbcf8a056152026ea5
eef <uberhaqer@gmail.com>
2012-05-07 01:25:56 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'mysql2'
+gem 'haml-rails'
+gem 'haml'
+gem 'jquery-rails'
+gem 'therubyracer'
+gem 'twitter-bootstrap-rails'
+gem 'simple-navigation'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citybam::Application.config.secret_token = '819dfef8a2efa877da8663502070808803d0591993ab2cb59a0debed1425f7c41acd3ce44e7d74891f2a4646e3c984086670b11ca3303cfbdc0ce874b36916dd'
