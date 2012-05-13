bf97bb25fc0b0d3d9489ebd7794794995bba49db
eef <uberhaqer@gmail.com>
2012-05-13 03:48:21 +0100

    updated css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.autocomplete
 //= require twitter/bootstrap
 //= require_tree .
--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,23 +1,23 @@
 $(function() {
 
-  var country_map = {"FR":"France", "GB":"United Kingdom", "JP":"Japan","IT":"Italy", "AU":"Australia", "NL":"Netherlands", "US":"United States", "ES":"Spain", "DE":"Germany", "HK":"Hong Kong", "SG":"Singapore", "BR":"Brazil", "CZ":"Czech Republic"}
+  // var country_map = {"FR":"France", "GB":"United Kingdom", "JP":"Japan","IT":"Italy", "AU":"Australia", "NL":"Netherlands", "US":"United States", "ES":"Spain", "DE":"Germany", "HK":"Hong Kong", "SG":"Singapore", "BR":"Brazil", "CZ":"Czech Republic"}
 
-	$.widget( "custom.catcomplete", $.ui.autocomplete, {
-		_renderMenu: function( ul, items ) {
-			var self = this,
-				currentCategory = "";
-			$.each( items, function( index, item ) {
-				if ( item.category != currentCategory ) {
-					ul.append( "<li class='ui-autocomplete-category'><a href='/cities/"+item.id+"'>" + country_map[item.country] + "</a></li>" );
-					currentCategory = item.category;
-				}
-				self._renderItem( ul, item );
-			});
-		}
-	});
+	// $.widget( "custom.catcomplete", $.ui.autocomplete, {
+	//   _renderMenu: function( ul, items ) {
+	//     var self = this,
+	//       currentCategory = "";
+	//     $.each( items, function( index, item ) {
+	//       if ( item.category != currentCategory ) {
+	//         ul.append( "<li class='ui-autocomplete-category'><a href='/cities/"+item.id+"'>" + country_map[item.country] + "</a></li>" );
+	//         currentCategory = item.category;
+	//       }
+	//       self._renderItem( ul, item );
+	//     });
+	//   }
+	// });
 
-  $( "#city-search" ).catcomplete({
-    source: "/cities/search"
-  });
+  // $( "#city-search" ).catcomplete({
+  //   source: "/cities/search"
+  // });
 
 });
