bbabe4b09f25f71059b5cef8916ff9bd5d773f9c
eef <uberhaqer@gmail.com>
2012-05-14 06:03:45 +0100

    bleh

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Citybam::Application.routes.draw do
 
   resources :launch_alerts, :only => :create
 
+  resources :venues
+
   resources :home, :only => [:index]
+
+  resources :cities do
+    get :search, :on => :collection
+  end
 
   root :to => 'home#index'
 
