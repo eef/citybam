0de21d007ac434b16a47575be9a5711670bf9f42
eef <uberhaqer@gmail.com>
2012-05-24 00:40:02 +0100

    added frontpage map with geolocation stuff

---
+++ app/assets/javascripts/location.js
@@ -0,0 +1,37 @@
+jQuery(window).ready(function(){
+
+  initiate_geolocation();
+
+});
+
+function initiate_geolocation() {
+  navigator.geolocation.getCurrentPosition(handle_geolocation_query,handle_errors);
+}
+
+function handle_errors(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED: alert("user did not share geolocation data");
+    break;
+
+    case error.POSITION_UNAVAILABLE: alert("could not detect current position");
+    break;
+
+    case error.TIMEOUT: alert("retrieving position timed out");
+    break;
+
+    default: alert("unknown error");
+    break;
+  }
+}
+
+function handle_geolocation_query(position) {
+    console.log("whit")
+    var myOptions = {
+    center: new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+}
--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,8 @@ module Citybam
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.gmaps_api_key = "AIzaSyB7uHN2e7YLqgo_Gxh24ONCHOPV9eGtdYI"
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
